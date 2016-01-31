87a62aea82a9af4bf71d469549670e7bb258ac46
David Francos Cuartero <dfrancos@buguroo.com>
2016-01-31 15:24:37 +0100

    Renamed to cursed_desktop

---
+++ cursed_desktop/__init__.py
@@ -0,0 +1,112 @@
+import os
+import urwid
+import click
+import tmuxp
+from collections import defaultdict
+from xdg.DesktopEntry import DesktopEntry
+from cursedxdg.tui import CascadingBoxes, menu, sub_menu, menu_button,\
+    exit_program
+
+
+class CursedXDG(object):
+    """
+        CursedXDG
+    """
+
+    def __init__(self, appdir="/usr/share/applications/", palette=False,
+                 executor="tmux", filter_terminal=True):
+        self.appdir = appdir
+        self.filter_terminal = filter_terminal
+        self.apps = []
+        for path, _, files in os.walk(appdir):
+            self.apps.extend([os.path.join(path, f)
+                              for f in files if f.endswith('desktop')])
+        self.palette = palette
+        self.windows = [False]
+        if not self.palette:
+            self.palette = [
+                ('banner', 'black', 'light gray'),
+                ('streak', 'black', 'dark red'),
+                ('bg', 'black', 'dark blue'), ]
+        self.execute = getattr(self, executor)
+
+    @property
+    def categorized_menus(self):
+        """
+            Read the menus and return them in an array
+        """
+        result = defaultdict(list)
+        for entry in [DesktopEntry(entry) for entry in self.apps]:
+            try:
+                if self.filter_terminal and not entry.getTerminal():
+                    continue
+                category = entry.getCategories().pop()
+                result[category].append(entry)
+            except IndexError:
+                # HAHA! My past self tought that uncategorized items
+                # were not worth it!
+                pass
+        return result
+
+    def tmux(self, _, what):
+        """
+            Gets current session
+        """
+        server = tmuxp.Server()
+        cid = "${}".format(os.environ['TMUX'].split(',')[-1])
+        session = [s for s in server.sessions if s['session_id'] == cid][0]
+        window = session.new_window()
+        window.panes[-1].send_keys(what)
+
+    def classic(self, _, what):
+        """
+            Execute
+        """
+        return os.system(what)
+
+    @property
+    def menu_tree(self):
+        """
+            Magically build a menu tree.
+            This yields curses objects, so here be dragons.
+        """
+        exit_btn = menu_button('Exit', '', exit_program)
+
+        for menu_category, items in self.categorized_menus.items():
+            cat = []
+            for item in items:
+                cat.append(menu_button(item.getName(), item.getExec(),
+                                       self.execute))
+            yield sub_menu(menu_category, cat, self.windows)
+
+        yield exit_btn
+
+    def main(self):
+        self.windows[0] = CascadingBoxes(menu('Main menu', self.menu_tree))
+
+        urwid.MainLoop(self.windows[0], self.palette).run()
+
+
+@click.command()
+@click.option('--appdir', default="/usr/share/applications/",
+              help="Where to find .desktop files")
+@click.option('--executor', default='tmux',
+              help='How to run selected program',
+              type=click.Choice(['tmux', 'classic']))
+@click.option('--filter_terminal/--no-filter_terminal', default=True)
+def main(appdir=False, executor=False, filter_terminal=True):
+    """
+        Option management
+    """
+    if executor == "tmux":
+        if not os.environ['TMUX']:
+            raise Exception("In order to use tmux integration, "
+                            "this must be executed from INSIDE a "
+                            "running tmux session")
+
+    return CursedXDG(appdir, executor=executor,
+                     filter_terminal=filter_terminal).main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ cursed_desktop/tui.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urwid
+
+"""
+    Ncurses stuffs are always ugly.
+
+    So... this goes here, hopefully unnoticed by most people.
+
+                 __________/%%%%=
+                /%%%%%%%%%/%%%%=
+          =/%%%/%%%%%%%%%/%%%%=
+         =/%%%/%%%%%%%%%/%%%%=
+        =/%%%/%%%%%%%%%/%%%%=
+       =/%%%/%%%%%%%%%/%%%%=
+      =/%%%/%%%%%%%%%/
+     =/%%%/
+
+    (a carpet, got from: http://www.retrojunkie.com/asciiart/myth/carpets.htm )
+    (Yes, that is a terrible joke about me hiding things under the carpet)
+
+    Apparently my past me decided max_box_levels was going to be 4 by default.
+    Have that in mind if you intend to use this code for anything useful
+    that is not a normal menu based on xdg (wich usually should be around 3
+    levels)
+
+    Oh! Cool, after documenting and re-learning what this does, it's actually
+    not so bad.
+"""
+
+
+def menu_button(caption, program, callback):
+    """
+        Generate a menu button and assign it to a callback.
+        That is, in this case either:
+            - A call to current exec stuff
+            - A submenu change
+    """
+    button = urwid.Button(caption)
+    urwid.connect_signal(button, 'click', callback, program)
+    return urwid.AttrMap(button, None, focus_map='reversed')
+
+
+def sub_menu(caption, choices, windows):
+    """
+        Submenu, it creates a menu, and a button to open
+        it with a callback.
+    """
+    contents = menu(caption, choices)
+
+    def open_menu(button, foo=False):
+        """
+            Ta-Da!
+        """
+        return windows[0].open_box(contents)
+
+    return menu_button([caption, ''], False, open_menu)
+
+
+def menu(title, choices):
+    """
+        Main menu.
+    """
+    body = [urwid.Text(title), urwid.Divider()]
+    body.extend(choices)
+    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+
+
+def exit_program(button, none):
+    """
+        GTFO
+    """
+    raise urwid.ExitMainLoop()
+
+
+class CascadingBoxes(urwid.WidgetPlaceholder):
+    """
+        Main placeholder
+    """
+    max_box_levels = 4
+
+    def __init__(self, box):
+        super(CascadingBoxes, self).__init__(urwid.SolidFill())
+        self.box_level = 0
+        self.open_box(box)
+
+    def open_box(self, box):
+        """
+           Some magic happens here.
+           It opens a box, with a lot of hardcoded + calculated
+           positions, relative to previous open box.
+           Once we close a box, we decrement the current box count.
+           This way we get a nice "animation" on cascading windows
+           (hence the class name)
+        """
+        self.original_widget = urwid.Overlay(
+            urwid.LineBox(box),
+            self.original_widget,
+            align='center', width=('relative', 40),
+            valign='middle', height=('relative', 40),
+            min_width=24, min_height=8,
+            left=self.box_level * 3,
+            right=(self.max_box_levels - self.box_level - 1) * 3,
+            top=self.box_level * 2,
+            bottom=(self.max_box_levels - self.box_level - 1) * 2)
+        urwid.AttrMap(self.original_widget, 'bg')
+
+        self.box_level += 1
+
+    def keypress(self, size, key):
+        """
+            Override default keypress so we can close cascading boxes AND
+            decrement current box level
+        """
+        if key == 'esc' and self.box_level > 1:
+            self.original_widget = self.original_widget[0]
+            self.box_level -= 1
+        else:
+            return super(CascadingBoxes, self).keypress(size, key)
